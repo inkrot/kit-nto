aae9d5417097e8e17a05e56f8a99dc4fbea44168
Islam Zaripov <islam.zar@yandex.ru>
2019-10-08 22:29:23 +0300

    first commit

---
+++ 1/JavaTextEditor/src/ru/inkrot/kit/texteditor/Main.java
@@ -0,0 +1,13 @@
+package ru.inkrot.kit.texteditor;
+
+public class Main {
+
+    public Main() {
+        Screen screen = new Screen();
+        screen.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
---
+++ 1/JavaTextEditor/src/ru/inkrot/kit/texteditor/Screen.java
@@ -0,0 +1,105 @@
+package ru.inkrot.kit.texteditor;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class Screen extends JFrame implements ActionListener {
+
+    public final String ACTION_NEW = "ACTION_NEW";
+    public final String ACTION_OPEN = "ACTION_OPEN";
+    public final String ACTION_SAVE = "ACTION_SAVE";
+
+    public static File currentFile = null;
+
+    private JTextArea textArea;
+
+    public Screen() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setSize(700, 400);
+        setLocationRelativeTo(null);
+
+        initGui();
+    }
+
+    private void initGui() {
+        Font font = new Font("Arial", 0, 20);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Файл");
+        fileMenu.setFont(font);
+
+        JMenuItem newBtn = new JMenuItem("Новый");
+        newBtn.setFont(font);
+        newBtn.addActionListener(this);
+        newBtn.setActionCommand(ACTION_NEW);
+        fileMenu.add(newBtn);
+        //////////
+        JMenuItem openBtn = new JMenuItem("Открыть");
+        openBtn.setFont(font);
+        openBtn.addActionListener(this);
+        openBtn.setActionCommand(ACTION_OPEN);
+        fileMenu.add(openBtn);
+        //////////
+        JMenuItem saveBtn = new JMenuItem("Сохранить");
+        saveBtn.setFont(font);
+        saveBtn.addActionListener(this);
+        saveBtn.setActionCommand(ACTION_SAVE);
+        fileMenu.add(saveBtn);
+
+        textArea = new JTextArea();
+        textArea.setFont(font);
+        textArea.setBorder(BorderFactory.createCompoundBorder(
+                textArea.getBorder(),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+        textArea.setBounds(0, 0, 700, 400);
+
+        add(textArea, BorderLayout.CENTER);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+        String action = e.getActionCommand();
+        if (action.equals(ACTION_NEW)) {
+
+        } else if (action.equals(ACTION_OPEN)) {
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                    "Выберите текстовый файл", "txt");
+            chooser.setFileFilter(filter);
+            int returnVal = chooser.showOpenDialog(this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                currentFile = chooser.getSelectedFile();
+                setTitle(chooser.getSelectedFile().getAbsolutePath());
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new FileReader(currentFile));
+                    textArea.setText(reader.readLine());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } else if (action.equals(ACTION_SAVE)) {
+            if (currentFile != null) {
+                try {
+                    BufferedWriter writer = new BufferedWriter(
+                            new FileWriter(currentFile));
+                    writer.write(textArea.getText());
+                    writer.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+}
