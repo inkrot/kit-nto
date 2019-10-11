3ef2036d72fd5e3620ac8f69467cd8e108b18f34
Islam Zaripov <islam.zar@yandex.ru>
2019-10-12 00:01:28 +0300

    2 Лаба на Java - готово

--- 2/JavaDocEditor/src/main/java/ru/inkrot/kit/doceditor/Screen.java
+++ 2/JavaDocEditor/src/main/java/ru/inkrot/kit/doceditor/Screen.java
@@ -36,9 +36,9 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
     //public int currentFileTextSize = 0;           textArea.getFont().getSize()
     //public Color currentFileTextColor = null;     textArea.getForeground()
 
-    public boolean edited = false;
+    private boolean edited = false;
 
-    public static File currentFile = null;
+    private static File currentFile = null;
 
     private JTextArea textArea;
 
@@ -130,7 +130,7 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         fileMenu.add(closeBtn);
 
         menuBar.add(fontCombo = new JComboBox(new String[]{"Шрифт: Arial", "Шрифт: Calibri", "Шрифт: Comic Sans MS", "Шрифт: Impact", "Шрифт: Times New Roman"}));
-        menuBar.add(sizeCombo = new JComboBox(new String[]{"Размер: 8", "Размер: 12", "Размер: 16", "Размер: 20", "Размер: 24"}));
+        menuBar.add(sizeCombo = new JComboBox(new String[]{"Размер: 8", "Размер: 12", "Размер: 16", "Размер: 20", "Размер: 24", "Размер: 26"}));
         menuBar.add(colorCombo = new JComboBox(new String[]{"Цвет: черный", "Цвет: синий", "Цвет: желтый", "Цвет: зеленый", "Цвет: красный"}));
 
         fontCombo.addItemListener(this);
@@ -153,7 +153,9 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
             @Override
             public void keyReleased(KeyEvent e) {
                 if (loadedFileText != null) {
-                    if (! loadedFileText.equals(textArea.getText())) setEdited(true);
+                    String text = textArea.getText();
+                    if (text == null) text = "";
+                    if (! loadedFileText.equals(text)) setEdited(true);
                     else setEdited(false);
                 }
             }
@@ -168,16 +170,21 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         JComboBox c = (JComboBox) e.getSource();
         String option = (String) c.getSelectedItem();
         if (c.equals(fontCombo)) {
-            String value = option.substring(7);
-            textArea.setFont(new Font(value, 0, textArea.getFont().getSize()));
+            String font = option.substring(7);
+            textArea.setFont(new Font(font, 0, textArea.getFont().getSize()));
+            if (! loadedFileTextFont.equals(font)) setEdited(true);
+            else setEdited(false);
         } else if (c.equals(sizeCombo)) {
-            String value = option.substring(8);
-            textArea.setFont(new Font(textArea.getFont().getFamily(), 0, Integer.parseInt(value)));
+            int size = Integer.parseInt(option.substring(8));
+            textArea.setFont(new Font(textArea.getFont().getFamily(), 0, size));
+            if (loadedFileTextSize != size) setEdited(true);
+            else setEdited(false);
         } else if (c.equals(colorCombo)) {
-            String value = option.substring(6);
-            textArea.setForeground(strToColor(value));
+            Color color = strToColor(option.substring(6));
+            textArea.setForeground(color);
+            if (! loadedFileTextColor.equals(color)) setEdited(true);
+            else setEdited(false);
         }
-        setEdited(true);
     }
 
     @Override
@@ -185,19 +192,25 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         String action = e.getActionCommand();
         if (action.equals(ACTION_NEW)) {
             if (! checkIsSaved()) return;
+
             currentFile = null;
-            setTitle("Новый файл");
-            textArea.setText("");
-            edited = false;
+            loadedFileTextFont = "Calibri";
+            loadedFileTextColor = Color.BLACK;
+            loadedFileTextSize = 24;
             loadedFileText = "";
+            textArea.setFont(new Font(loadedFileTextFont, 0, loadedFileTextSize));
+            textArea.setForeground(loadedFileTextColor);
+            textArea.setText(loadedFileText);
+            fontCombo.setSelectedItem("Шрифт: " + loadedFileTextFont);
+            sizeCombo.setSelectedItem("Размер: " + loadedFileTextSize);
+            colorCombo.setSelectedItem("Цвет: " + colorToStr(loadedFileTextColor));
+
+            textArea.setText(loadedFileText);
+            setEdited(false);
             openEdit();
         } else if (action.equals(ACTION_OPEN)) {
             if (! checkIsSaved()) return;
 
-            loadWord();
-            openEdit();
-
-            /*
             JFileChooser chooser = new JFileChooser();
             chooser.setDialogTitle("Открыть");
             FileNameExtensionFilter filter = new FileNameExtensionFilter("Документ", "doc", "docx");
@@ -205,36 +218,25 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
             chooser.setAcceptAllFileFilterUsed(false);
             int returnVal = chooser.showOpenDialog(this);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
-                if (! chooser.getSelectedFile().exists()) {
+                if (!chooser.getSelectedFile().exists()) {
                     JOptionPane.showMessageDialog(null, "Файл не найден");
                     return;
                 }
-                currentFile = chooser.getSelectedFile();
-                setTitle(chooser.getSelectedFile().getAbsolutePath());
-                try {
-                    BufferedReader reader = new BufferedReader(new FileReader(currentFile));
-                    String line;
-                    StringBuilder sb = new StringBuilder();
-                    while ((line = reader.readLine()) != null) sb.append(line).append('\n');
-                    String text = sb.toString().substring(0, sb.length() - 1);
-                    textArea.setText(text);
-                    loadedFileText = text;
-                    openEdit();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            }*/
+                loadWord(chooser.getSelectedFile());
+                openEdit();
+            }
         } else if (action.equals(ACTION_SAVE)) {
             saveCurrentFile();
         } else if (action.equals(ACTION_CLOSE)) {
             if (! checkIsSaved()) return;
             textArea.setText("");
-            setEdited(false);
             closeEdit();
         }
     }
 
-    public void setEdited(boolean edited) {
+    private void setEdited(boolean edited) {
+        edited = edited || !loadedFileText.equals(textArea.getText());
+        this.edited = edited;
         if (edited) {
             if (currentFile != null) Screen.this.setTitle(currentFile.getAbsolutePath() + " *");
             else Screen.this.setTitle("Новый файл *");
@@ -242,10 +244,10 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
             if (currentFile != null) Screen.this.setTitle(currentFile.getAbsolutePath());
             else Screen.this.setTitle("Новый файл");
         }
-        this.edited = edited;
     }
 
-    public void closeEdit() {
+    private void closeEdit() {
+        edited = false;
         setTitle(TITLE);
         fontCombo.setVisible(false);
         sizeCombo.setVisible(false);
@@ -254,7 +256,7 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         textArea.setEnabled(false);
     }
 
-    public void openEdit() {
+    private void openEdit() {
         fontCombo.setVisible(true);
         sizeCombo.setVisible(true);
         colorCombo.setVisible(true);
@@ -262,25 +264,58 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         textArea.setEnabled(true);
     }
 
-    public void writeWord() {
-
+    private void writeWord(File file) {
+        try {
+            XWPFDocument document = new XWPFDocument();
+            FileOutputStream out = new FileOutputStream(file);
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun run = paragraph.createRun();
+            loadedFileTextFont = textArea.getFont().getFontName();
+            loadedFileTextSize = textArea.getFont().getSize();
+            loadedFileTextColor = textArea.getForeground();
+            loadedFileText = textArea.getText();
+            setEdited(false);
+            run.setFontFamily(loadedFileTextFont);
+            run.setFontSize(loadedFileTextSize);
+            Color c = loadedFileTextColor;
+            run.setColor(String.format("%02X%02X%02X", c.getRed(), c.getGreen(), c.getBlue()));
+            for (String line : textArea.getText().split("\n")) {
+                run.setText(line);
+                run.addBreak();
+            }
+            document.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void loadWord() {
-        File file = new File("C:/Users/Islam/Desktop/kit-nto\\2\\JavaDocEditor\\src\\main\\resources\\document.docx");
+    private void loadWord(File file) {
         try {
             currentFile = file;
             FileInputStream fis = new FileInputStream(file);
-            XWPFDocument doc = new XWPFDocument(fis);
-            List<XWPFParagraph> paragraphs = doc.getParagraphs();
-            XWPFParagraph paragraph = paragraphs.get(0);
-            XWPFRun run = paragraph.getRuns().get(0);
-
-            loadedFileTextFont = run.getFontFamily();
-            loadedFileTextColor = Color.decode("#" + run.getColor());
-            loadedFileTextSize = run.getFontSize();
-            loadedFileText = paragraph.getText();
-
+            XWPFDocument doc = null;
+            try {
+                doc = new XWPFDocument(fis);
+                List<XWPFParagraph> paragraphs = doc.getParagraphs();
+                XWPFParagraph paragraph = paragraphs.get(0);
+                XWPFRun run = paragraph.getRuns().get(0);
+                loadedFileTextFont = run.getFontFamily();
+                if (run.getColor() != null) loadedFileTextColor = Color.decode("#" + run.getColor());
+                else loadedFileTextColor = Color.BLACK;
+                loadedFileTextSize = run.getFontSize();
+                loadedFileText = paragraph.getText();
+                textArea.setFont(new Font(loadedFileTextFont, 0, loadedFileTextSize));
+                textArea.setForeground(loadedFileTextColor);
+                textArea.setText(loadedFileText);
+            } catch (Exception e) {
+                loadedFileTextFont = "Calibri";
+                loadedFileTextColor = Color.BLACK;
+                loadedFileTextSize = 24;
+                loadedFileText = "";
+                setEdited(false);
+                return;
+            }
             textArea.setFont(new Font(loadedFileTextFont, 0, loadedFileTextSize));
             textArea.setForeground(loadedFileTextColor);
             textArea.setText(loadedFileText);
@@ -316,7 +351,7 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
     // return: true - продолжить; false - отмена
     private boolean checkIsSaved()
     {
-        if (!edited) return true;
+        if (! edited) return true;
         String what = currentFile == null ? "новый файл" : "\"" + currentFile.getAbsolutePath() + "\"";
         int result = JOptionPane.showConfirmDialog(null, "Сохранить " + what + "?", TITLE, JOptionPane.YES_NO_CANCEL_OPTION);
         if (result == JOptionPane.YES_OPTION) {
@@ -327,34 +362,24 @@ public class Screen extends JFrame implements ActionListener, ItemListener {
         } else return false;
     }
 
-    public void saveCurrentFile() {
+    private void saveCurrentFile() {
         if (currentFile != null) {
-            writeFile(currentFile, textArea.getText());
-            setTitle(currentFile.getAbsolutePath());
-            edited = false;
+            writeWord(currentFile);
+            setEdited(false);
         } else {
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setDialogTitle("Сохранить файл");
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("Документ (*.docx)", "docx", "doc");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Документ (*.docx)", "docx");
             fileChooser.setAcceptAllFileFilterUsed(false);
             fileChooser.addChoosableFileFilter(filter);
             int userSelection = fileChooser.showSaveDialog(this);
             if (userSelection == JFileChooser.APPROVE_OPTION) {
                 File selectedFile = fileChooser.getSelectedFile();
                 String path = selectedFile.getPath();
-                currentFile = new File(path.endsWith(".txt") ? path : path + ".txt");
-                writeFile(currentFile, textArea.getText());
-                setTitle(currentFile.getAbsolutePath());
-                edited = false;
+                currentFile = new File(path.endsWith(".docx") ? path : path + ".docx");
+                writeWord(currentFile);
+                setEdited(false);
             }
-        }
-    }
-
-    public void writeFile(File file, String text) {
-        try (PrintWriter out = new PrintWriter(file)) {
-            out.println(text);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
 }
